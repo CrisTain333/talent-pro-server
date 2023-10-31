345372e3113145434b6d562413c6882fd2e0f6b1
sakib-repliq <sakibul.islam@repliq.dev>
2023-10-31 23:20:22 +0600

    server setup complete

---
+++ server.js
@@ -0,0 +1,22 @@
+const mongoose = require("mongoose");
+require("dotenv").config();
+
+const app = require("./app");
+
+const port = process.env.PORT || 5000;
+const uri = process.env.DATABASE_URI;
+
+async function main() {
+  try {
+    await mongoose.connect(uri);
+    console.log("Database connection successful");
+  } catch (error) {
+    console.log("Failed to connect to the database", error);
+  }
+}
+
+app.listen(port, () => {
+  console.log(`Application listening on port ${port}`);
+});
+
+main();
