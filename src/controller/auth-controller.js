11e27dca8ed284f99a7a67a4a9f1700ffc69b842
Sukanta Das <arnobh84@gmail.com>
2023-11-01 20:15:42 +0600

    register api completed and ctachAsync Shared Function added

--- src/controller/auth-controller.js
+++ src/controller/auth-controller.js
@@ -1,22 +1,21 @@
 const authService = require("../services/auth-service");
+const catchAsync = require("../shared/catchAsync");
 const sendResponse = require("../shared/sendResponse");
-const registerUser = async (req, res, next) => {
-  try {
-    const data = req.body;
-    const result = await authService.handleRegisterUser(
-      data
-    );
-    sendResponse(res, {
-      data: {
-        token: result?.token,
-      },
-      statusCode: 200,
-      message: "Registered successfully",
-    });
-  } catch (error) {
-    next(error);
-  }
-};
+
+// ** Register
+const registerUser = catchAsync(async (req, res) => {
+  const { ...registerData } = req.body;
+  const result = await authService.handleRegisterUser(
+    registerData
+  );
+  sendResponse(res, {
+    data: {
+      token: result?.token,
+    },
+    statusCode: 200,
+    message: "Account Registered successfully",
+  });
+});
 
 module.exports = {
   registerUser,
--- src/services/auth-service.js
+++ src/services/auth-service.js
@@ -5,45 +5,36 @@ const { User_Role } = require("../constant/user-roles");
 const ApiError = require("../error/ApiError");
 
 exports.handleRegisterUser = async (userData) => {
-  try {
-    // Check if the user already exists
-    const existingUser = await User.findOne({
-      $or: [{ email: userData.email }],
-    });
+  // Check if the user already exists
+  const existingUser = await User.findOne({
+    $or: [{ email: userData.email }],
+  });
 
-    if (existingUser) {
-      throw new ApiError(400, "Email already in use");
-    }
-    // Hash the plain password
-    const hashedPassword = await bcrypt.hash(
-      userData.password,
-      10
-    );
-
-    userData.role = User_Role.CANDIDATE;
-    userData.password = hashedPassword;
-
-    const result = await User.create(userData);
-
-    const payload = {
-      role: userData.role,
-      id: result.id,
-    };
-
-    const token = jwt.sign(
-      payload,
-      process.env.JWT_SECRET,
-      {
-        expiresIn: process.env.JWT_EXPIRES_IN,
-      }
-    );
-
-    // send success message
-    return {
-      token,
-    };
-  } catch (error) {
-    console.error(error);
-    throw new ApiError(400, "Failed to create user");
+  if (existingUser) {
+    throw new ApiError(400, "Email already in use");
   }
+  // Hash the plain password
+  const hashedPassword = await bcrypt.hash(
+    userData.password,
+    10
+  );
+
+  userData.role = User_Role.CANDIDATE;
+  userData.password = hashedPassword;
+
+  const result = await User.create(userData);
+
+  const payload = {
+    role: userData.role,
+    id: result.id,
+  };
+
+  const token = jwt.sign(payload, process.env.JWT_SECRET, {
+    expiresIn: process.env.JWT_EXPIRES_IN,
+  });
+
+  // send success message
+  return {
+    token,
+  };
 };
