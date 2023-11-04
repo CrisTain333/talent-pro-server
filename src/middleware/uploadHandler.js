07ae90c72c051044d3d8600e860a4d4ac9a005ce
Sukanta Das <arnobh84@gmail.com>
2023-11-04 15:02:34 +0600

    file uploader middleware added

---
+++ src/middleware/uploadHandler.js
@@ -0,0 +1,46 @@
+const cloudinary = require('cloudinary').v2;
+
+// Configuration
+cloudinary.config({
+    cloud_name: process.env.CLOUD_NAME,
+    api_key: process.env.CLOUD_API_KEY,
+    api_secret: process.env.CLOUD_API_SECRET
+});
+
+async function uploadFiles(files) {
+    console.log(files);
+
+    try {
+        const uploadResults = [];
+
+        // Loop through the files and upload each one
+        for (const file of files) {
+            if (file?.fieldname === 'profile-picture') {
+                const uploadResult =
+                    await cloudinary.uploader.upload(
+                        file.path,
+                        {
+                            folder: 'Talent-Pro/profile-pictures'
+                        }
+                    );
+                uploadResults.push(uploadResult.secure_url);
+            } else if (file?.fieldname === 'resume') {
+                const uploadResult =
+                    await cloudinary.uploader.upload(
+                        file.path,
+                        {
+                            folder: 'Talent-Pro/documents'
+                        }
+                    );
+                uploadResults.push(uploadResult.secure_url);
+            }
+        }
+
+        return uploadResults;
+    } catch (error) {
+        console.error('Error uploading files:', error);
+        throw error;
+    }
+}
+
+module.exports = { uploadFiles };
