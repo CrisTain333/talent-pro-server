59a0fbbca1887cc2df3fe4c1335c33ae1e5f8699
Sukanta Das <arnobh84@gmail.com>
2023-11-04 18:41:43 +0600

    some change again in uploader

--- src/shared/uploadFile.js
+++ src/shared/uploadFile.js
@@ -1,11 +1,14 @@
 const cloudinary = require('cloudinary').v2;
-
+require('dotenv').config();
 // Configuration
 cloudinary.config({
     cloud_name: process.env.CLOUD_NAME,
     api_key: process.env.CLOUD_API_KEY,
     api_secret: process.env.CLOUD_API_SECRET
 });
+console.log(process.env.CLOUD_NAME);
+console.log(process.env.CLOUD_API_KEY);
+console.log(process.env.CLOUD_API_SECRET);
 
 async function uploadFiles(file) {
     console.log(file);
