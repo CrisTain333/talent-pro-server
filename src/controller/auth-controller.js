f7d67bc50c76002ac6168b0540961e74bc44802c
Sukanta Das <arnobh84@gmail.com>
2023-11-13 09:14:31 +0600

    register response  format changed

--- src/controller/auth-controller.js
+++ src/controller/auth-controller.js
@@ -1,13 +1,22 @@
 const authService = require('../services/auth-service');
 const catchAsync = require('../shared/catchAsync');
+const sendResponse = require('../shared/sendResponse');
 
 const register = catchAsync(async (req, res) => {
     const { ...registerData } = req.body;
     const result =
         await authService.handleRegister(registerData);
-    res.status(201).json({
-        access: result?.token
+
+    sendResponse(res, {
+        statusCode: 201,
+        message: 'Registered successfully',
+        data: {
+            access: result?.token
+        }
     });
+    // res.status(201).json({
+    //     access: result?.token
+    // });
 });
 
 const token = catchAsync(async (req, res) => {
