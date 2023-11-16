8b9c0503a2e50107cee979978ff064beae0f8acb
Sukanta Das <arnobh84@gmail.com>
2023-11-16 12:02:37 +0600

    created new api for start server

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -3,8 +3,10 @@ const router = express.Router();
 
 const authRoute = require('../api/auth/index');
 const meRoute = require('../api/me/index');
+const { startServer } = require('../api/server');
 
 router.use('/auth', authRoute);
 router.use('/me', meRoute);
+router.use('/start', startServer);
 
 module.exports = router;
