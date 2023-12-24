e43d2356611954157364dda8fe2805d261fba6d5
Sukanta Das <arnobh84@gmail.com>
2023-12-24 14:31:43 +0600

    test added for auth get me routes

--- src/__tests__/auth.test.js
+++ src/__tests__/auth.test.js
@@ -13,31 +13,33 @@ const {
 } = require('mongodb-memory-server');
 const { default: mongoose } = require('mongoose');
 const app = require('../app');
-// let app;
 
-describe('Auth Tests', () => {
+let TOKEN;
+
+describe('Auth', () => {
+    let mongod;
+    const dummyUser = {
+        name: {
+            first_name: 'John',
+            last_name: 'Doe'
+        },
+        email: 'john.doe@example.com',
+        password: 'securepassword',
+        role: 'candidate',
+        account_status: 'active',
+        isOnboardComplete: true,
+        image_url: 'https://example.com/avatar.jpg'
+    };
     beforeAll(async () => {
-        const mongod = await MongoMemoryServer.create();
+        mongod = await MongoMemoryServer.create();
         const uri = mongod.getUri();
         await mongoose.connect(uri);
     });
     afterAll(async () => {
         await mongoose.disconnect();
+        await mongod.stop();
     });
     describe('register', () => {
-        const dummyUser = {
-            name: {
-                first_name: 'John',
-                last_name: 'Doe'
-            },
-            email: 'john.doe@example.com',
-            password: 'securepassword',
-            role: 'candidate',
-            account_status: 'active',
-            isOnboardComplete: true,
-            image_url: 'https://example.com/avatar.jpg'
-        };
-
         it('should return 201 and Access Token', async () => {
             const { statusCode, body } = await supertest(
                 app
@@ -47,8 +49,41 @@ describe('Auth Tests', () => {
                 .set('Accept', 'application/json');
 
             expect(statusCode).toBe(201);
+            console.log(body);
             expect(body?.result?.access).toBeDefined();
-            // console.log(body);
+        });
+    });
+    describe('login', () => {
+        const payload = {
+            email: 'john.doe@example.com',
+            password: 'securepassword'
+        };
+
+        it('should return 200 and Access Token', async () => {
+            const { statusCode, body } = await supertest(
+                app
+            )
+                .post(`/api/v1/auth/token`)
+                .send(payload)
+                .set('Accept', 'application/json');
+
+            TOKEN = body?.result?.access;
+            expect(statusCode).toBe(200);
+            expect(body?.result?.access).toBeDefined();
+        });
+    });
+    describe('Get own profile', () => {
+        it('should return user', async () => {
+            const { statusCode, body } = await supertest(
+                app
+            )
+                .get(`/api/v1/me`)
+                .set('Authorization', `Bearer ${TOKEN}`)
+                .expect(200);
+
+            console.log(body);
+            expect(statusCode).toBe(200);
+            expect(body?.result).toBeTruthy();
         });
     });
 });
