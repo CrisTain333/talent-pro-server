07536fe72ff311cba00f70760822c69565c435fa
sakib-xrz <sakibxrz21@gmail.com>
2023-12-25 14:39:56 +0600

    add timestamps to all model

--- src/model/candidateModel.js
+++ src/model/candidateModel.js
@@ -5,70 +5,78 @@ const {
     IndustryOptions
 } = require('../constant/keyChain');
 
-const candidateProfileSchema = new mongoose.Schema({
-    user_id: {
-        type: mongoose.Schema.Types.ObjectId,
-        ref: 'User', // Reference to the User model
-        required: true
-    },
-    desired_salary: {
-        min: {
-            type: Number,
+const candidateProfileSchema = new mongoose.Schema(
+    {
+        user_id: {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: 'User', // Reference to the User model
             required: true
         },
-        max: {
-            type: Number,
+        desired_salary: {
+            min: {
+                type: Number,
+                required: true
+            },
+            max: {
+                type: Number,
+                required: true
+            }
+        },
+        industry: {
+            type: String,
+            enum: IndustryOptions,
+            required: true
+        },
+        phone: {
+            type: String,
+            required: true
+        },
+        location: {
+            type: String,
+            required: true
+        },
+
+        job_status: {
+            type: String,
+            enum: EmployStatus,
+            required: true
+        },
+
+        skills: {
+            type: [
+                {
+                    label: {
+                        type: String
+                    },
+
+                    value: {
+                        type: String
+                    }
+                }
+            ],
+            required: true
+        },
+        portfolio: {
+            type: String,
+            required: true
+        },
+        resume: {
+            type: String,
+            required: true
+        },
+
+        open_to_work_remotely: {
+            type: Boolean,
             required: true
         }
     },
-    industry: {
-        type: String,
-        enum: IndustryOptions,
-        required: true
-    },
-    phone: {
-        type: String,
-        required: true
-    },
-    location: {
-        type: String,
-        required: true
-    },
-
-    job_status: {
-        type: String,
-        enum: EmployStatus,
-        required: true
-    },
-
-    skills: {
-        type: [
-            {
-                label: {
-                    type: String
-                },
-
-                value: {
-                    type: String
-                }
-            }
-        ],
-        required: true
-    },
-    portfolio: {
-        type: String,
-        required: true
-    },
-    resume: {
-        type: String,
-        required: true
-    },
-
-    open_to_work_remotely: {
-        type: Boolean,
-        required: true
+    {
+        timestamps: true,
+        toJSON: {
+            virtuals: true
+        }
     }
-});
+);
 
 const Candidate = mongoose.model(
     'Candidate',
--- src/model/organizationModel.js
+++ src/model/organizationModel.js
@@ -1,50 +1,58 @@
 const mongoose = require('mongoose');
 const { IndustryOptions } = require('../constant/keyChain');
 
-const organizationSchema = new mongoose.Schema({
-    user_id: {
-        type: mongoose.Schema.Types.ObjectId,
-        ref: 'User',
-        required: true
+const organizationSchema = new mongoose.Schema(
+    {
+        user_id: {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: 'User',
+            required: true
+        },
+        company_logo: {
+            type: String,
+            default:
+                'https://res.cloudinary.com/talent-pro/image/upload/v1703379624/organization_placeholder.png'
+        },
+        company_name: {
+            type: String,
+            required: true
+        },
+        industry: {
+            type: String,
+            enum: IndustryOptions,
+            required: true
+        },
+        company_location: {
+            type: String,
+            required: true
+        },
+        company_size: {
+            type: String,
+            required: true
+        },
+        website: {
+            type: String,
+            required: true
+        },
+        linkedin_link: {
+            type: String
+        },
+        slogan: {
+            type: String,
+            required: true
+        },
+        about_us: {
+            type: String,
+            required: true
+        }
     },
-    company_logo: {
-        type: String,
-        default:
-            'https://res.cloudinary.com/talent-pro/image/upload/v1703379624/organization_placeholder.png'
-    },
-    company_name: {
-        type: String,
-        required: true
-    },
-    industry: {
-        type: String,
-        enum: IndustryOptions,
-        required: true
-    },
-    company_location: {
-        type: String,
-        required: true
-    },
-    company_size: {
-        type: String,
-        required: true
-    },
-    website: {
-        type: String,
-        required: true
-    },
-    linkedin_link: {
-        type: String
-    },
-    slogan: {
-        type: String,
-        required: true
-    },
-    about_us: {
-        type: String,
-        required: true
+    {
+        timestamps: true,
+        toJSON: {
+            virtuals: true
+        }
     }
-});
+);
 
 const Organization = mongoose.model(
     'organization',
