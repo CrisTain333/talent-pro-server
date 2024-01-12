81bbcd1e68ada874b891bb4e51eec214dfff5728
sakib-repliq <sakibul.islam@repliq.dev>
2024-01-12 23:03:13 +0600

    application model chnages

--- src/model/applicationModel.js
+++ src/model/applicationModel.js
@@ -6,91 +6,90 @@ const {
     applicationStatus
 } = require('../constant/keyChain');
 
-const applicationSchema = new mongoose.Schema({
-    user_id: {
-        type: mongoose.Schema.Types.ObjectId,
-        ref: 'User', // Reference to the User model
-        required: true
-    },
-    candidate: {
-        type: mongoose.Schema.Types.ObjectId,
-        ref: 'Candidate', // Reference to the Candidate model
-        required: true
-    },
-    job: {
-        _id: {
+const applicationSchema = new mongoose.Schema(
+    {
+        user_id: {
             type: mongoose.Schema.Types.ObjectId,
+            ref: 'User', // Reference to the User model
+            required: true
+        },
+        candidate: {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: 'Candidate', // Reference to the Candidate model
             required: true
         },
         organization: {
-            company_logo: {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: 'Organization', // Reference to the Organization model
+            required: true
+        },
+        job: {
+            _id: {
+                type: mongoose.Schema.Types.ObjectId,
+                required: true
+            },
+            job_title: {
                 type: String,
                 required: true
             },
-            company_name: {
+            job_type: {
                 type: String,
+                enum: EmploymentType,
                 required: true
             },
-            company_location: {
+            experience_level: {
                 type: String,
+                enum: ExperienceLevel,
+                required: true
+            },
+            location_type: {
+                type: String,
+                enum: LocationType,
                 required: true
             }
         },
-        job_title: {
+        phone: {
             type: String,
             required: true
         },
-        job_type: {
-            type: String,
-            enum: EmploymentType,
+        years_of_experience: {
+            type: Number,
             required: true
         },
-        experience_level: {
-            type: String,
-            enum: ExperienceLevel,
+        skills: {
+            type: [
+                {
+                    label: {
+                        type: String,
+                        required: true
+                    },
+
+                    value: {
+                        type: String,
+                        required: true
+                    }
+                }
+            ],
             required: true
         },
-        location_type: {
+        resume: {
             type: String,
-            enum: LocationType,
+            required: true
+        },
+        status: {
+            type: String,
+            enum: applicationStatus,
+            default: 'Application Received',
             required: true
         }
     },
-    phone: {
-        type: String,
-        required: true
-    },
-    years_of_experience: {
-        type: Number,
-        required: true
-    },
-    skills: {
-        type: [
-            {
-                label: {
-                    type: String,
-                    required: true
-                },
-
-                value: {
-                    type: String,
-                    required: true
-                }
-            }
-        ],
-        required: true
-    },
-    resume: {
-        type: String,
-        required: true
-    },
-    status: {
-        type: String,
-        enum: applicationStatus,
-        default: 'Application Received',
-        required: true
+    {
+        timestamps: true,
+        toJSON: {
+            virtuals: true
+        }
     }
-});
+);
 
 const Application = mongoose.model('Application', applicationSchema);
 
