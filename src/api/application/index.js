b9d95443962e93441657ce7239f7e58d359472b4
cristain333 <sukanta.das4014@gmail.com>
2024-01-13 22:02:24 +0600

    changein api

--- src/api/application/index.js
+++ src/api/application/index.js
@@ -3,9 +3,12 @@ const router = express.Router();
 const applicationController = require('../../controller/application-controller');
 const auth = require('../../middleware/auth');
 const { User_Role } = require('../../constant/user-roles');
+const uploader = require('../../middleware/uploader');
 router.post(
     '/',
     auth(User_Role.CANDIDATE),
+    uploader.single('resume'),
+
     applicationController.applyJobController
 );
 
