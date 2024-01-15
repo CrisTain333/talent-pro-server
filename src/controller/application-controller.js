6dbc0d9430620501b4d50c50e20e305df19ce770
cristain333 <sukanta.das4014@gmail.com>
2024-01-15 09:41:10 +0600

    get all applied jobs with pagination and search,filter completed

--- src/controller/application-controller.js
+++ src/controller/application-controller.js
@@ -36,8 +36,8 @@ const getAppliedJob = catchAsync(async (req, res) => {
         statusCode: 200,
         success: true,
         message: `applied job's retrieved successfully`,
-        data: result
-        // meta: result.meta
+        data: result.data,
+        meta: result.meta
     });
 });
 
